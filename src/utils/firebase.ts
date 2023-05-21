1657e7648a7405d90b7287216da56cbd8a612cc6
Santipr <113911133+Santipr29@users.noreply.github.com>
2023-05-21 15:45:15 -0500

    Feat: Delete firebase config

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,12 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCI1veTV4rnpeg-Fn220rwVvDh1ihOpf4U",
-    authDomain: "dca-perez.firebaseapp.com",
-    projectId: "dca-perez",
-    storageBucket: "dca-perez.appspot.com",
-    messagingSenderId: "116722604539",
-    appId: "1:116722604539:web:62b7afc51095347ef1535e",
-    measurementId: "G-M6EX5SP9ML"
-  };
 
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, getDocs,onSnapshot, doc, serverTimestamp,query,orderBy} from "firebase/firestore";
